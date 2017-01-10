4421da77d55db9d089481ffc29d255981fd6db1f
Ryan Whittingham <whittingham777@hotmail.com>
2017-01-09 23:03:55 -0800

    Basic Structuring

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+//array of quotes with the following properties, quote, source, citation, and year
+var quotes = [
+
+	['I have no money, no resources, no hopes.  I am the happiest man alive', 
+	'Henry Miller', 
+	'Tropic of Cancer',
+	'1925', 
+	],
+
+];
+
+
+//function that selects random quote object from quotes array and returns the data
+function getRandomQuote(){
+
+
+};
+
+//function that grabs getRandomQuote data, formats, and displays it to web page
+function printQuote(){
+	
+	document.getElementById('quote-box').innerHTML
+
+};
+
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
