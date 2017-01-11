5ac216493ec39aa0748a4d77e6d7d3ecaa3eb45d
Ryan Whittingham <whittingham777@hotmail.com>
2017-01-10 23:56:13 -0800

    Functional Script
    Script works but might need refinement.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,79 @@
-//array of quotes with the following properties, quote, source, citation, and year
+//array of quotes with the following properties: quote, source, citation, and year
 var quotes = [
 
 	['I have no money, no resources, no hopes.  I am the happiest man alive', 
 	'Henry Miller', 
 	'Tropic of Cancer',
-	'1925', 
+	'1925' 
+	],
+
+	['You will become way less concerned with what other people think of you when you realize how seldom they do.', 
+	'David Foster Wallace',
+	'Infinite Jest' 
+	],
+
+	['I tell you, we are here on Earth to fart around, and don\'t let anybody tell you different', 
+	'Kurt Vonnegut'
 	],
 
 ];
 
 
+
 //function that selects random quote object from quotes array and returns the data
 function getRandomQuote(){
 
+	//variable to track the number of quotes in the quotes array
+	var numberOfQuotes = quotes.length;
+	//console.log(numberOfQuotes);
+
+	//variable that creates random number between 0 and number of quotes
+	var randomNumber = Math.floor(Math.random() * numberOfQuotes);
+	//console.log(randomNumber);
+	
+	return quotes[randomNumber];
 
 };
 
-//function that grabs getRandomQuote data, formats, and displays it to web page
+
+
+//function that grabs getRandomQuote data, checks the number of elements, formats with html, and displays it to web page 
 function printQuote(){
 	
-	document.getElementById('quote-box').innerHTML
+	var newQuote=getRandomQuote();
+
+	if (newQuote.length === 2) {
+		
+		document.getElementById('quote-box').innerHTML =
+
+		'<p class="quote">' + newQuote[0] + '</p>' +
+		'<p class="source">' + newQuote[1]  +
+		'</p>'
+
+	}
+
+	else if (newQuote.length === 3) {
+		
+		document.getElementById('quote-box').innerHTML =
+
+		'<p class="quote">' + newQuote[0] + '</p>' +
+		'<p class="source">' + newQuote[1]  +
+			'<span class="citation">' + newQuote[2] + '</span>' +
+		'</p>'
+
+	}
+
+	else {
+		
+		document.getElementById('quote-box').innerHTML =
+
+		'<p class="quote">' + newQuote[0] + '</p>' +
+		'<p class="source">' + newQuote[1]  +
+			'<span class="citation">' + newQuote[2] + '</span>' +
+			'<span class="year">' + newQuote[3] + '</span>' +
+		'</p>'
+
+	}
 
 };
 
